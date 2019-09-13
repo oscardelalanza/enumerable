5da0aa4f47667d7f95b0f9abfedccfaa0c3ad78d
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-13 13:41:56 -0700

    rubocop rules modified and method definitions

--- enumerable.rb
+++ enumerable.rb
@@ -1,1 +1,23 @@
+# frozen_string_literal: true
 
+module Enumerable
+
+    def my_each; end
+
+    def my_each_width_index; end
+
+    def my_select; end
+
+    def my_all; end
+
+    def my_any; end
+
+    def my_none; end
+
+    def my_count; end
+
+    def my_map; end
+
+    def my_inject; end
+
+end
