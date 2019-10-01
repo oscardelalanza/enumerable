2a6e3445d53c111eb5832547307b047f91246e65
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-01 13:25:12 -0700

    context #my_each test added to check compatibility with blocks

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Enumerable do
             expect(array_new).to eql([1, 2, 3, 4])
         end
         
+        it 'accepts blocks and apply it to all the elements' do
+            array.my_each { |x| array_new.push(x * 5) }
+            expect(array_new).to eql([5, 10, 15, 20])
+        end
+        
     end
     
 end
